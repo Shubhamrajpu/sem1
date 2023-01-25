75fecdf3c060ca44315a14db9bcdf2f1f8e63040
Shubhamrajpu <98015960+Shubhamrajpu@users.noreply.github.com>
2023-01-25 21:34:41 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+print("=================================")
+print("your Name");
+input("");
+print("your enrollment no");
+input("");
+print("Branch");
+input("");
+print("age");
+input("");
+print("EMAIL ID");
+input("");
+print("mobile no");
+input("");
